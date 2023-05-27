66b25d6e288e7d757145c21503b045d4f1fc53f8
SiropTime <sirop4beats@gmail.com>
2023-05-27 23:47:37 +0300

    [~] json to string

--- internal/calculations/model/logic.go
+++ internal/calculations/model/logic.go
@@ -3,17 +3,17 @@ package model
 type BasicCompanyLogic struct {
 	ProjectName       *string  `json:"project_name"`
 	OrganizationType  *string  `json:"organization_type"`
-	WorkersQuantity   *int     `json:"workers_quantity"`
+	WorkersQuantity   *int     `json:"workers_quantity,string"`
 	Industry          *string  `json:"industry"`
 	County            *string  `json:"county"`
-	LandArea          *int     `json:"land_area"`
-	BuildingArea      *int     `json:"building_area"`
+	LandArea          *int     `json:"land_area,string"`
+	BuildingArea      *int     `json:"building_area,string"`
 	MachineNames      []string `json:"machine_names"`
-	MachineQuantities []int    `json:"machine_quantities"`
+	MachineQuantities []int    `json:"machine_quantities,string"`
 	PatentType        *string  `json:"patent_type"`
 	Bookkeeping       *bool    `json:"bookkeeping"`
 	TaxSystem         *string  `json:"tax_system"`
-	Operations        *int     `json:"operations"`
+	Operations        *int     `json:"operations,string"`
 	OtherNeeds        []string `json:"other_needs"`
 }
 

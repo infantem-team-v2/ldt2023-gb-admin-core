01e08a02e81a19c8b34681badcc595d00e8bd552
SiropTime <sirop4beats@gmail.com>
2023-05-27 20:04:36 +0300

    [~] ff

--- internal/calculations/usecase/usecase.go
+++ internal/calculations/usecase/usecase.go
@@ -17,32 +17,9 @@ func BuildCalculationsUseCase(ctn di.Container) (interface{}, error) {
 }
 
 func (cu *CalculationsUseCase) BaseCalculate(params *model.BaseCalculateRequest) (*model.BaseCalculateResponse, error) {
-	filteredInputs := make([]*model.BasicCalculationFieldLogic, 0, len(params.Inputs))
-	for _, inputs := range params.Inputs {
-		if inputs.Value != nil {
-			filteredInputs = append(filteredInputs, inputs)
-		}
-	}
 
-	return &model.BaseCalculateResponse{
-		InputData: []*model.BasicCalculationFieldLogic{
-			{
-				Field: "Отрасль",
-				Value: "Фармацевтика",
-			},
-		},
-		OutputData: []*model.BasicCategoryCalculationLogic{
-			{
-				Category: "Персонал",
-				Data: []*model.BasicCalculationFieldLogic{
-					{
-						Field: "Затраты на зарплату",
-						Value: "100 тыс. руб.",
-					},
-				},
-			},
-		},
-	}, nil
+	return &model.BaseCalculateResponse{}, nil
+
 }
 
 func (cu *CalculationsUseCase) ImprovedCalculate(params *model.BaseCalculateRequest) (*model.ImprovedCalculateResponse, error) {
